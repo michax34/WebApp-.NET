0c7c925263a19e9af4c402a5515f9c338e5c1698
michax34 <michaxplaygames@gmail.com>
2024-03-25 16:35:53 +0100

    no message

---
+++ WebApplication1/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var objCatrgoryList = _db.categories.ToList();
+            return View();
+        }
+    }
+}
